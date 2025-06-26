e05150e6758e7aebd1e6fe2f131093e8792fb916
Land Maze <61432929+Land-Maze@users.noreply.github.com>
2025-06-26 03:54:23 +0200

    [WEB] Changed the main page layout

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,10 +8,10 @@ const blog = defineCollection({
 	schema: ({ image }) => z.object({
 		title: z.string(),
 		description: z.string(),
-		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
-		heroImage: image().optional(),
+		image: image().optional(),
+		tags: z.array(z.string()).optional(),
 	}),
 });
 
