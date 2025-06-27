6916b4b9e89260badeef08767bb6a12d024b9433
Land Maze <61432929+Land-Maze@users.noreply.github.com>
2025-06-27 02:56:04 +0200

    [WEB] Added mobile optimization, fixed index layout, added github icon

--- src/content.config.ts
+++ src/content.config.ts
@@ -11,6 +11,8 @@ const blog = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		image: image().optional(),
+		imageAlt: z.string().optional(),
+		imageCaption: z.string().optional(),
 		tags: z.array(z.string()).optional(),
 	}),
 });
